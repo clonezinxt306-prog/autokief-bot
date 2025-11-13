306599ade09638211b5dbd874f4a491c893f9b50
clonezinxt306-prog <clonezinxt306@gmail.com>
2025-11-12 21:44:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,6 @@ async def painel(interaction: discord.Interaction):
 
 # Inicie o bot
 import os
-bot.run(os.getenv('DISCORD_TOKEN')
+bot.run(os.getenv('MTQzNzA5MDMzOTkxODgzOTk4MA.Gj8Wdr.TINK7Xxno5gQpu3k0isJ9cGjCdYYwVOmQOYY9A')
+
 
